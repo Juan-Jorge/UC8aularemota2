40f6dca2ccea78d0ecc2d629db172a1526adad1d
Juan-Jorge <juan>
2021-10-23 23:29:30 -0300

    Oba

---
+++ lista.js
@@ -0,0 +1,14 @@
+let numerodealunos = 5
+for(let contador = 0; contador < numerodealunos; contador++) {
+    console.log(contador)
+    
+    if(contador == 0) {
+    console.log("Zero")
+} else if (contador % 2 == 1){
+    console.log ("Impar") 
+} else {
+    console.log("Par")
+}
+
+}
+
